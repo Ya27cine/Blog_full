b756e7b81ce1da4ec4be240bab39d20f734cb013
KHELIFA Yassine <prostam27@gmail.com>
2021-10-18 23:25:21 +0200

    creating New Post : View update

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,7 +1,12 @@
 <?php 
  namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,10 +25,30 @@ use Symfony\Component\Routing\Annotation\Route;
      /**
       * @Route("/blog/create", name="blog-create")
       */
-      public function create()
+      public function create(Request $request)
       {
+        $em   = $this->getDoctrine()->getManager();
+        $post = new Post;
+
+        $form = $this->createFormBuilder( $post )
+                ->add("title", TextType::class)
+                ->add("content", TextareaType::class)
+                ->add("create", SubmitType::class, ['label' => "New Post"])
+                ->getForm();
+        
+        $form->handleRequest( $request );
+        if( $form->isSubmitted() && $form->isValid() && 0 ){
+           
+            $em->persist( $post);
+            $em->flush();
+
+            return $this->redirectToRoute('blog-create');
+        }
+
+
          return $this->render('blog/create.html.twig', [
-             'data' => 'New Post'
+             'data' => 'New Post',
+             'mForm' => $form->createView()
          ]);
       }
  }
