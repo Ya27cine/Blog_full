1407fbbad79b785e01a19da4ee6441bd484bcdc6
KHELIFA Yassine <prostam27@gmail.com>
2021-10-18 22:07:53 +0200

    install template

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,30 @@
+<?php 
+ namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+ class BlogController extends AbstractController
+ {
+     /**
+      * @Route("/blog", name="blog-index")
+      */
+     public function index()
+     {
+        return $this->render('blog/index.html.twig', [
+            'data' => 'all-posts'
+        ]);
+     }
+
+     /**
+      * @Route("/blog/create", name="blog-create")
+      */
+      public function create()
+      {
+         return $this->render('blog/show.html.twig', [
+             'data' => 'New Post'
+         ]);
+      }
+ }
+?>
