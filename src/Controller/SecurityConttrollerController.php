9e199de8c2b98498de7ea624ecc3a7599b5c0156
KHELIFA Yassine <prostam27@gmail.com>
2021-10-24 11:21:27 +0200

    save object form regist, messing validtor

--- src/Controller/SecurityConttrollerController.php
+++ src/Controller/SecurityConttrollerController.php
@@ -5,19 +5,27 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use  Doctrine\ORM\EntityManagerInterface;
 
 class SecurityConttrollerController extends AbstractController
 {
     /**
      * @Route("/registration", name="security_registration")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $objectManager)
     {
         $user = new User();
 
         $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $objectManager->persist( $user );
+            $objectManager->flush();
+        }
 
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView(),
