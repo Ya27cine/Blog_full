52178685e7d02342c3b9ab3b711717f0eff60460
KHELIFA Yassine <prostam27@gmail.com>
2021-10-24 12:37:07 +0200

    valid: unique-entity EMAIL

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,13 +4,16 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 
-
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ *  fields = { "email" }
+ * )
  */
 class User implements UserInterface
 {
