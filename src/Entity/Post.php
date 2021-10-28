6bdad92dd8fd2a8af424914d64519fa08c8c306d
KHELIFA Yassine <prostam27@gmail.com>
2021-10-28 22:27:07 +0200

    create entity category, and rel with Post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -37,6 +37,11 @@ class Post
      */
     private $user;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="category")
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +91,18 @@ class Post
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
